f2e35d40f38c858189816d3d40586a4d2b4a90cc
Song Keum Hyun <kumh1999@gmail.com>
2021-11-13 16:17:42 +0900

    외부에서 Config 값 가져오기 예제

--- SongMainStart/src/main/java/com/song/HolomanRunner.java
+++ SongMainStart/src/main/java/com/song/HolomanRunner.java
@@ -1,8 +1,10 @@
 package com.song;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import com.example.Holoman;
@@ -13,11 +15,30 @@ public class HolomanRunner implements ApplicationRunner {
 	@Autowired
 	Holoman holoman;
 	
+	@Autowired
+	SongProperties songProperties;
+	
+
+	@Value("${song.name}")
+	private String name;
+	
+	@Autowired
+	private Environment environment;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
 		
+		System.out.println("사용자 플로퍼티 만들어서 가져오기 " + songProperties.toString());
+		
+		System.out.println("환경변수에서 온값 " + environment.getProperty("song.name").toString());
+		
 		System.out.println("++++++++++" + holoman.toString());
+		
+		System.out.println("프로퍼티에서 가져 온값 " + name);
+		
+
+		
 		
 	}
 
--- SongMainStart/src/main/java/com/song/SongMainStartApplication.java
+++ SongMainStart/src/main/java/com/song/SongMainStartApplication.java
@@ -1,13 +1,17 @@
 package com.song;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SongMainStartApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SongMainStartApplication.class, args);
+
+		
 	}
 
 }
