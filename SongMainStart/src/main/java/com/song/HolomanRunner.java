d92dd2377374597bc36eeed76fe0eb10e2ebb592
Song Keum Hyun <kumh1999@gmail.com>
2021-11-13 16:49:55 +0900

    Logging 추가

--- SongMainStart/src/main/java/com/song/HolomanRunner.java
+++ SongMainStart/src/main/java/com/song/HolomanRunner.java
@@ -1,5 +1,8 @@
 package com.song;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -8,6 +11,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import com.example.Holoman;
+
 
 @Component
 public class HolomanRunner implements ApplicationRunner {
@@ -24,18 +28,21 @@ public class HolomanRunner implements ApplicationRunner {
 	
 	@Autowired
 	private Environment environment;
+	
+	private Logger logger = LoggerFactory.getLogger(HolomanRunner.class);
+		
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
+		logger.debug("=================================================");
+		logger.debug("사용자 플로퍼티 만들어서 가져오기 " + songProperties.toString());
 		
-		System.out.println("사용자 플로퍼티 만들어서 가져오기 " + songProperties.toString());
+		logger.debug("환경변수에서 온값 " + environment.getProperty("song.name").toString());
 		
-		System.out.println("환경변수에서 온값 " + environment.getProperty("song.name").toString());
+		logger.debug("++++++++++" + holoman.toString());
 		
-		System.out.println("++++++++++" + holoman.toString());
-		
-		System.out.println("프로퍼티에서 가져 온값 " + name);
+		logger.debug("프로퍼티에서 가져 온값 " + name);
 		
 
 		
